d595dea0372636ccaf3ea161a52504f78bebd062
Romuald <romualdescande@gmail.com>
2017-01-24 11:46:36 +0000

    corrected one test

--- test/integration/item_test.rb
+++ test/integration/item_test.rb
@@ -8,9 +8,8 @@ class ItemTest < ActionDispatch::IntegrationTest
     fill_in "item_name", with: "Chaise"
     fill_in "item_value", with: "30"
     click_button 'Create Item'
-    save_and_open_screenshot
+    # save_and_open_screenshot
 
-    # puts Item.last
     # assert_equal new_item_share_path(Item.last), page.current_path
     assert_equal 200, page.status_code
     # assert page.has_content?("Add a friend sharing ownership of this item with you")
