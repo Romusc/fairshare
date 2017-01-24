627ff1f349ccf56726aa58b8987e39aeb26a625a
Romuald <romualdescande@gmail.com>
2017-01-24 12:11:32 +0000

    Changed the test on homepage

--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -8,7 +8,6 @@ class HomeTest < ActionDispatch::IntegrationTest
     visit "/"
 
     # verify
-    assert_equal 200, page.status_code
     assert page.has_content?("Items")
     assert page.has_selector?(".item", count: Item.count)
 
