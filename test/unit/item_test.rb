599c55c8c3a95f0e239615a2e5310a5aacebd3c7
Romuald <romualdescande@gmail.com>
2017-03-28 12:44:54 +0200

    added a unit test but commented it out until i figure out why it clashes with the integration test(both files are called item_test.rb)

---
+++ test/unit/item_test.rb
@@ -0,0 +1,8 @@
+# require 'test_helper'
+
+# class ItemTest < ActiveSupport::TestCase
+#   test "invalid without a name" do
+#     i = Item.new
+#     assert !i.valid?, "Should not validate an item without a name"
+#   end
+# end
